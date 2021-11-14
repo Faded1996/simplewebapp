26d071985ddfbd5de547a3e790efef5dd027a21d
bogdan <bogdan.mazur.bmy@gmail.com>
2021-11-14 15:00:11 +0300

    Minor changes in Test/service

--- src/test/java/com/mastery/java/task/service/EmployeeServiceTest.java
+++ src/test/java/com/mastery/java/task/service/EmployeeServiceTest.java
@@ -8,19 +8,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
-@ExtendWith(SpringExtension.class)
 public class EmployeeServiceTest {
 
-    @MockBean
+    @Mock
     private EmployeeDao employeeDao;
 
     @InjectMocks
